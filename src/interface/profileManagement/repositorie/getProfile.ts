c40aa92dc707e5cee72b6f52f9202f9de9406710
LuizFelipeM <muniz.mourafelipe@gmail.com>
2020-08-31 18:47:46 -0300

    GraphQL last steps

--- src/interface/profileManagement/repositorie/getProfile.ts
+++ src/interface/profileManagement/repositorie/getProfile.ts
@@ -3,11 +3,17 @@ import { IProfile } from '../../../domain/interfaces/profile/IProfile'
 import { rulesInProfilesTableName } from '../../../respository/common/rulesInProfilesTable'
 import { knex } from '../../../dataAccess/database/knex/dbConnection'
 
-export const getProfile = async (id: string): Promise<IProfile> => await profilesTable
-  .innerJoin(rulesInProfilesTableName, 't1.id', `${rulesInProfilesTableName}.idProfile`)
-  .select(
-    't1.id',
-    knex.raw(`ARRAY_AGG(${rulesInProfilesTableName}."idRule") as rules`)
-  )
-  .where('t1.id', id)
-  .groupBy('t1.id')
+export const getProfile = async (id: string): Promise<IProfile> => {
+  const prof = await profilesTable
+    .innerJoin(rulesInProfilesTableName, 't1.id', `${rulesInProfilesTableName}.idProfile`)
+    .select<IProfile>(
+      't1.*',
+      knex.raw(`ARRAY_AGG(${rulesInProfilesTableName}."idRule") as rules`)
+    )
+    .where({ id: '75ccf9c0-dcf4-47ac-ad19-49593e1004e8' })
+    .groupBy('t1.id')
+
+  console.log('prof', prof)
+
+  return prof
+}

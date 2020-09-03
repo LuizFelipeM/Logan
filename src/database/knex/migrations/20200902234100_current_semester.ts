169ac87b413ff5f79ae0b192f0f70cdf9333fa55
tiofeels <lucas.daniel42@hotmail.com>
2020-09-02 23:53:32 -0300

    Migration of Current Semester

---
+++ src/database/knex/migrations/20200902234100_current_semester.ts
@@ -0,0 +1,32 @@
+import * as Knex from 'knex'
+
+export async function up (knex: Knex): Promise<void> {
+  return knex.schema.createTableIfNotExists('currentSemester', function (table) {
+    table.increments('id').primary()// PK of table Current Semester
+
+    table.integer('idCalender').notNullable()
+    table.integer('idDicipline').notNullable()
+
+    table.timestamp('createdAt', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('lastUpdate', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.timestamp('evalP1Start', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('evalP1End', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.timestamp('evalP2Start', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('evalP2End', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.timestamp('evalSubStart', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('evalSubEnd', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.timestamp('evalExamStart', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('evalExamEnd', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.foreign('idCalender').references('id').inTable('calender')
+    table.foreign('idDicipline').references('id').inTable('dicipline')
+  })
+}
+
+export async function down (knex: Knex): Promise<void> {
+  return knex.schema.dropSchemaIfExists('statusRegistry')
+}

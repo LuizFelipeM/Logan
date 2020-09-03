d39da3b69c1dd916d346fcd9a476a3ca7057fe11
Pedro Ricciardi <Pedrohenriquericciardi@gmail.com>
2020-09-03 13:23:47 -0300

    Tabela calendario academico

---
+++ src/database/knex/migrations/20200902133053_academiccalendar.ts
@@ -0,0 +1,26 @@
+import * as Knex from 'knex'
+
+export async function up (knex: Knex): Promise<void> {
+  return knex.schema.createTableIfNotExists('academicCalendar', function (table) {
+    table.integer('id').primary()
+
+    table.timestamp('StartAcademicYear', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('AcademicYearEnd', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.timestamp('StartNotesP1', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('FinalNotesP1', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.timestamp('StartNotesP2', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('FinalNotesP2', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.timestamp('StartNotesSub', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('FinalNotesSub', { precision: 6 }).defaultTo(knex.fn.now(6))
+
+    table.timestamp('StartNotesExam', { precision: 6 }).defaultTo(knex.fn.now(6))
+    table.timestamp('FinalNotesExam', { precision: 6 }).defaultTo(knex.fn.now(6))
+  })
+}
+
+export async function down (knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('academicCalendar')
+}

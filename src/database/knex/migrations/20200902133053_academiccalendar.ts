d58886b2d59628ef4471c1d75e54c128f7686330
Pedro Ricciardi <Pedrohenriquericciardi@gmail.com>
2020-09-04 22:17:51 -0300

    AGORA SIM A TABELA CALENDAR ESTA ATUALIZADA

--- src/database/knex/migrations/20200902133053_academiccalendar.ts
+++ src/database/knex/migrations/20200902133053_academiccalendar.ts
@@ -7,20 +7,20 @@ export async function up (knex: Knex): Promise<void> {
     table.timestamp('createdAt', { precision: 6 }).notNullable()
     table.timestamp('lastUpdate', { precision: 6 }).notNullable()
 
-    table.timestamp('StartAcademicYear', { precision: 6 }).notNullable()
-    table.timestamp('AcademicYearEnd', { precision: 6 }).notNullable()
+    table.timestamp('startAcademicYear', { precision: 6 }).notNullable()
+    table.timestamp('academicYearEnd', { precision: 6 }).notNullable()
 
-    table.timestamp('StartNotesP1', { precision: 6 }).notNullable()
-    table.timestamp('FinalNotesP1', { precision: 6 }).notNullable()
+    table.timestamp('startNotesP1', { precision: 6 }).notNullable()
+    table.timestamp('finalNotesP1', { precision: 6 }).notNullable()
 
-    table.timestamp('StartNotesP2', { precision: 6 }).notNullable()
-    table.timestamp('FinalNotesP2', { precision: 6 }).notNullable()
+    table.timestamp('startNotesP2', { precision: 6 }).notNullable()
+    table.timestamp('finalNotesP2', { precision: 6 }).notNullable()
 
-    table.timestamp('StartNotesSub', { precision: 6 }).notNullable()
-    table.timestamp('FinalNotesSub', { precision: 6 }).notNullable()
+    table.timestamp('startNotesSub', { precision: 6 }).notNullable()
+    table.timestamp('finalNotesSub', { precision: 6 }).notNullable()
 
-    table.timestamp('StartNotesExam', { precision: 6 }).notNullable()
-    table.timestamp('FinalNotesExam', { precision: 6 }).notNullable()
+    table.timestamp('startNotesExam', { precision: 6 }).notNullable()
+    table.timestamp('finalNotesExam', { precision: 6 }).notNullable()
   })
 }
 

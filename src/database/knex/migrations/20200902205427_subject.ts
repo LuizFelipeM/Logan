0cb4d722572d275067be2a94d17d4f20cfdf292b
tiofeels <lucas.daniel42@hotmail.com>
2020-09-04 21:34:28 -0300

    Alteration names of FK for idProfessor and inTable for Professor

--- src/database/knex/migrations/20200902205427_subject.ts
+++ src/database/knex/migrations/20200902205427_subject.ts
@@ -2,11 +2,11 @@ import * as Knex from 'knex'
 
 export async function up (knex: Knex): Promise<void> {
   return knex.schema.createTableIfNotExists('subject', function (table) {
-    table.integer('idTeacher').notNullable()
+    table.integer('idProfessor').notNullable()
     table.integer('idDiscipline').notNullable()// Ask Pedro what he named here
     table.integer('idClasses').notNullable()
 
-    table.foreign('idTeacher').references('id').inTable('teachers')
+    table.foreign('idProfessor').references('id').inTable('professor')
     table.foreign('idDiscipline').references('id').inTable('discipline')
     table.foreign('idClasses').references('id').inTable('classes')
   })

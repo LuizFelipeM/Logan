bd768a74c61cb229542d002cf99795445808ba50
LuizFelipeM <muniz.mourafelipe@gmail.com>
2020-09-07 21:34:06 -0300

    Adding try catch to students controller

--- backend/src/controllers/studentController.ts
+++ backend/src/controllers/studentController.ts
@@ -5,29 +5,44 @@ import { IStudent } from '../domain/interfaces/IStudent'
 export const studentController = Router()
 
 studentController.get('/get-all', async (req: Request, res: Response) => {
-  const students = await studentService.getStudents()
+  try {
+    const students = await studentService.getStudents()
 
-  return res.json(students)
+    return res.json(students)
+  } catch (error) {
+    console.error(error)
+    return res.status(404).json({ error })
+  }
 })
 
 studentController.get('/get', async (req: Request, res: Response) => {
-  const { id, userId } = req.query
+  try {
+    const { id, userId } = req.query
 
-  let students: IStudent | undefined
+    let students: IStudent | undefined
 
-  if (id) {
-    students = await studentService.getStudentById(Number(id))
-  } else if (userId) {
-    students = await studentService.getStudentByUserId(Number(userId))
+    if (id) {
+      students = await studentService.getStudentById(Number(id))
+    } else if (userId) {
+      students = await studentService.getStudentByUserId(Number(userId))
+    }
+
+    return res.json(students)
+  } catch (error) {
+    console.error(error)
+    return res.status(404).json({ error })
   }
-
-  return res.json(students)
 })
 
 studentController.post('/', async (req: Request, res: Response) => {
-  const student: IStudent = req.body
+  try {
+    const student: IStudent = req.body
 
-  const createdStudent = await studentService.createStudent(student)
+    const createdStudent = await studentService.createStudent(student)
 
-  return res.json(createdStudent)
+    return res.json(createdStudent)
+  } catch (error) {
+    console.error(error)
+    return res.status(404).json({ error })
+  }
 })

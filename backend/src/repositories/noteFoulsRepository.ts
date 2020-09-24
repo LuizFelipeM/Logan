3d4afce4f17c196b51a55fc3d3b8887ec03b27f8
tiofeels <lucas.daniel42@hotmail.com>
2020-09-23 21:20:44 -0300

    Removing comments

--- backend/src/repositories/noteFoulsRepository.ts
+++ backend/src/repositories/noteFoulsRepository.ts
@@ -22,22 +22,4 @@ export class NoteFoulsRepository extends AbstractRepository<INoteFouls> {
       'u.firstName'
     )
     .where('s.ra', ra)
-
-  // selectByStudentId = async (students: number): Promise<INoteFouls> => await knex(noteFoulsTable)
-  //   .select('*')
-  //   .where({ students })
-  //   .first()
 }
-// const getNoteFoulByStudentId = async (students: number): Promise<INoteFouls> => await knex(noteFoulsTable)
-//   .select('*')
-//   .where({ students })
-//   .first()
-
-// const getNoteFouls = async (): Promise<INoteFouls[]> => await knex(noteFoulsTable)
-//   .select('*')
-
-// const insertNoteFouls = async (data: Omit<INoteFouls, 'id'>): Promise<INoteFouls> => await knex(noteFoulsTable)
-//   .insert(data)
-//   .returning<INoteFouls>('*')
-
-// export const noteFoulsRepository = { getNoteFouls, insertNoteFouls }
--- backend/src/services/noteFoulsService.ts
+++ backend/src/services/noteFoulsService.ts
@@ -19,14 +19,3 @@ export class NoteFoulsService extends AbstractService<INoteFouls, NoteFoulsRepos
     }
   }
 }
-
-// const getNoteFouls = async (): Promise<INoteFouls[]> => await noteFoulsRepository.getNoteFouls()
-// const getNoteFoulsByStudentId = async (id: number): Promise<INoteFouls> => await noteFoulsRepository.getNoteFoulByStudentId(id)
-
-// const createNoteFouls = async (noteFouls: Omit<INoteFouls, 'id'>): Promise<INoteFouls> => noteFoulsRepository.insertNoteFouls(noteFouls)
-
-// export const noteFoulsService = {
-//   getNoteFouls,
-//   // getNoteFoulsByStudentId,
-//   createNoteFouls
-// }

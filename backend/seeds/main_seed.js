9ce72611488039066d6f08b75db4eff6f4abba86
tiofeels <lucas.daniel42@hotmail.com>
2020-09-27 22:33:12 -0300

    Add Main_Seed

---
+++ backend/seeds/main_seed.js
@@ -0,0 +1,17 @@
+
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          firstName: 'Lucas',
+          lastName: 'Daniel',
+          gender: 'Male',
+          birthDate: '17/11/1999'
+        }
+
+      ])
+    })
+}

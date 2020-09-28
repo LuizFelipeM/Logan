f2c8b8a8f6d96a0c0e96ff8e53b18ace3154cbb4
tiofeels <lucas.daniel42@hotmail.com>
2020-09-28 19:09:19 -0300

    Change interfaces

--- backend/src/domain/interfaces/entities/ISemester.ts
+++ backend/src/domain/interfaces/entities/ISemester.ts
@@ -2,14 +2,14 @@ import { IBaseEntity } from './IBaseEntity'
 
 export interface ISemester extends IBaseEntity {
     currentSemester: number
-    calendar: number
-    discipline: number
-    evalP1Start: number,
-    evalP1end: number,
-    evalP2Start: number,
-    evalP2end: number,
-    evalSubStart: number,
-    evalSubEnd: number,
-    evalExamStart: number,
-    evalExamEnd: number
+    calendar: string
+    discipline: string
+    evalP1Start: string,
+    evalP1end: string,
+    evalP2Start: string,
+    evalP2end: string,
+    evalSubStart: string,
+    evalSubEnd: string,
+    evalExamStart: string,
+    evalExamEnd: string
 }

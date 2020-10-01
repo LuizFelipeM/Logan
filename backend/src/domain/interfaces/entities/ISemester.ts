4d621dec56d607d933b8209e1b840bfba0efd0c8
tiofeels <lucas.daniel42@hotmail.com>
2020-09-30 21:39:02 -0300

    Altered types of ISemester

--- backend/src/domain/interfaces/entities/ISemester.ts
+++ backend/src/domain/interfaces/entities/ISemester.ts
@@ -2,17 +2,6 @@ import { IBaseEntity } from './IBaseEntity'
 
 export interface ISemester extends IBaseEntity {
     currentSemester: number
-<<<<<<< HEAD
-    calendar: string
-    discipline: string
-    evalP1Start: string,
-    evalP1end: string,
-    evalP2Start: string,
-    evalP2end: string,
-    evalSubStart: string,
-    evalSubEnd: string,
-    evalExamStart: string,
-=======
     calendar: number
     discipline: number
     evalP1Start: string
@@ -22,6 +11,5 @@ export interface ISemester extends IBaseEntity {
     evalSubStart: string
     evalSubEnd: string
     evalExamStart: string
->>>>>>> develop
     evalExamEnd: string
 }

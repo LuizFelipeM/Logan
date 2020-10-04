cc618feb188fbb73fd57af0959751c519e884258
LuizFelipeM <muniz.mourafelipe@gmail.com>
2020-10-04 18:34:01 -0300

    Add the charting library React-Vis to project

--- client/src/panels/admin/pages/CourseManagement/index.tsx
+++ client/src/panels/admin/pages/CourseManagement/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { LineSeries, XYPlot } from 'react-vis'
 
-const CourseManagement: React.FC = () => <p>CourseManagement</p>
+const CourseManagement: React.FC = () => (
+  <XYPlot height={300} width={300}>
+    <LineSeries data={[
+      { x: 0, y: 8 },
+      { x: 1, y: 5 },
+      { x: 2, y: 4 },
+      { x: 3, y: 9 },
+      { x: 4, y: 1 },
+      { x: 5, y: 7 },
+      { x: 6, y: 6 },
+      { x: 7, y: 3 },
+      { x: 8, y: 2 },
+      { x: 9, y: 0 }
+    ]}
+    />
+  </XYPlot>
+)
 
 export default CourseManagement

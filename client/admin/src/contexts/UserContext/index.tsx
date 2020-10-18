771d50f6972eaddc460e38c7fd23efa7c06c6c72
LuizFelipeM <muniz.mourafelipe@gmail.com>
2020-10-18 13:09:43 -0300

    Removed react-vis dependency added Recharts dependency for charts components

--- client/admin/src/contexts/UserContext/index.tsx
+++ client/admin/src/contexts/UserContext/index.tsx
@@ -1,26 +1,31 @@
-import React, { createContext, ReactNode, useState } from 'react'
-import { IUser } from '../../interfaces/IUser'
+import React, { createContext, ReactNode, useState } from "react";
+import { IUser } from "../../interfaces/IUser";
 
 interface UserContextProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 interface Context {
-    currentUser: Partial<IUser> | undefined
-    setCurrentUser: React.Dispatch<React.SetStateAction<Partial<IUser> | undefined>>
+  currentUser: Partial<IUser> | undefined;
+  setCurrentUser: React.Dispatch<
+    React.SetStateAction<Partial<IUser> | undefined>
+  >;
 }
 
-const UserContext = createContext<Context>({ currentUser: undefined, setCurrentUser: () => {} })
-const { Provider } = UserContext
+const UserContext = createContext<Context>({
+  currentUser: undefined,
+  setCurrentUser: () => {},
+});
+const { Provider } = UserContext;
 
 const UserProvider: React.FC<UserContextProps> = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState<Partial<IUser> | undefined>(undefined)
+  const [currentUser, setCurrentUser] = useState<Partial<IUser> | undefined>(
+    undefined
+  );
 
   return (
-    <Provider value={{ currentUser, setCurrentUser }}>
-      {children}
-    </Provider>
-  )
-}
+    <Provider value={{ currentUser, setCurrentUser }}>{children}</Provider>
+  );
+};
 
-export { UserContext, UserProvider }
+export { UserContext, UserProvider };
--- client/admin/src/contexts/WrapperContext/index.tsx
+++ client/admin/src/contexts/WrapperContext/index.tsx
@@ -1,33 +1,32 @@
-import React, { createContext, ReactNode, useState } from 'react'
-import { Spinner } from 'react-bootstrap'
+import React, { createContext, ReactNode, useState } from "react";
+import { Spinner } from "react-bootstrap";
 
-import './style.scss'
+import "./style.scss";
 
 interface WrapperContextProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 interface Context {
-    setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const WrapperContext = createContext<Context>({ setLoading: () => {} })
-const { Provider } = WrapperContext
+const WrapperContext = createContext<Context>({ setLoading: () => {} });
+const { Provider } = WrapperContext;
 
 const WrapperProvider: React.FC<WrapperContextProps> = ({ children }) => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   return (
     <Provider value={{ setLoading }}>
-      {loading
-                && (
-                <div className="loading-container d-flex justify-content-center">
-                  <Spinner animation="border" variant="light" />
-                </div>
-                )}
+      {loading && (
+        <div className="loading-container d-flex justify-content-center">
+          <Spinner animation="border" variant="light" />
+        </div>
+      )}
       {children}
     </Provider>
-  )
-}
+  );
+};
 
-export { WrapperContext, WrapperProvider }
+export { WrapperContext, WrapperProvider };
